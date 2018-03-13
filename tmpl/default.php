617f7768d1c02d029c05cae53edbab1d156e0cfb
Tristan de Boer <info@tristandeboer.nl>
2018-03-13 10:07:11 +0100

    Added link in header

--- tmpl/default.php
+++ tmpl/default.php
@@ -2,9 +2,12 @@
 // No direct access
 defined('_JEXEC') or die; ?>
 <div class="flickrstream">
-  <div class="flickrstream-title" style="background-color: <?PHP echo $color; ?>">
-    <i class="icon-picture"></i> <?PHP echo $title; ?>
-  </div>
+  <a href="https://www.flickr.com/slideShow/index.gne?user_id=<?PHP echo $userId; ?>&amp;set_id=<?PHP echo $setId; ?>" target="_blank">
+    <div class="flickrstream-title" style="background-color: <?PHP echo $color; ?>">
+      <i class="icon-picture"></i> <?PHP echo $title; ?>
+      <span class="icon icon-chevron-right" style="float: right"></span>
+    </div>
+  </a>
   <div class="flickrstream-content">
     <div class="embed-responsive embed-responsive-16by9">
       <iframe src="https://www.flickr.com/slideShow/index.gne?user_id=<?PHP echo $userId; ?>&amp;set_id=<?PHP echo $setId; ?>" width="500" height="500" style="vertical-align: center;" frameborder="0" scrolling="no" align="center"></iframe>
